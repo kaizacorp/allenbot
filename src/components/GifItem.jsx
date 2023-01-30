73381591b19b6d093f3b58e653b805660fa5c577
Kai <kaicharlton@gmail.com>
2023-01-30 11:22:18 -0600

    make gifs in grid links to themselves

--- src/components/GifItem.jsx
+++ src/components/GifItem.jsx
@@ -4,7 +4,7 @@ import GifType from '../GifType'
 
 const GifItem = ({ gif }) => (
         <div className="gif-item" key={gif._id}>
-          <img src={gif.url} alt={gif.tags} title={gif.tags}></img>
+          <a href={gif.url}><img src={gif.url} alt={gif.tags} title={gif.tags}></img></a>
         </div>
 );
 
