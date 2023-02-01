6ff1efdbe05168054627cd6f0309acbd8dae6fc4
Kai <kaicharlton@gmail.com>
2023-02-01 15:57:34 -0600

    remove redundant comments, clearer component name

--- src/components/GifsGrid.jsx
+++ src/components/GifsGrid.jsx
@@ -2,10 +2,9 @@ import React, {useContext} from 'react';
 import GifsContext from '../GifsContext';
 import GifItem from './GifItem';
 
-// default component if no gifs with matching tags are found.
-const NoMatchingTags = ({tag}) => {
+const NoMatchingTagsFound = ({tag}) => {
   return (
-    <div id="default" className="filter-default" >
+    <div className="no-matching-tags-found">
     No matching tags found for: <br/> '{tag}'
     </div>
   );
@@ -16,18 +15,16 @@ const GifsGrid = () => {
       state: {gifs, filter},
     } = useContext(GifsContext);
 
-    // is a valid filter string if 2 characters or more after trimming
-    // single characters get too many matches, poor UX -> unable to search for gifs with "I" though
+    // single characters get too many matches, poor UX
+    // -> unable to search for gifs with "I" though, for example
     const cleanFilter = filter.toLowerCase().trim();
     const validFilter = cleanFilter.length > 1 ? cleanFilter : "";
     const GIF_LIMIT = 18;
     
     return (
         <div className="gif-grid">
-          {/* if after filtering there are no matching results, let the user know which query failed */}
-          {gifs.filter((gif) => (gif.tags.toLowerCase().includes(validFilter))).length ? null : <NoMatchingTags tag={filter}/>}
+          {gifs.filter((gif) => (gif.tags.toLowerCase().includes(validFilter))).length ? null : <NoMatchingTagsFound tag={filter}/>}
 
-          {/* show all gifs with tags matching filter (up to GIF_LIMIT) as GifItem components */}
           {gifs
             .filter((gif) =>
               gif.tags.toLowerCase().includes(validFilter)
